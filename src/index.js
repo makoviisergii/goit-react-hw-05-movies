e9ff69680421a7ca9b5dd3dfd166470dd5deb913
makoviisergii <sergiimakovii78@gmail.com>
2023-03-20 20:43:30 +0200

    20.03 20.15

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { AuthProvider } from './hoc/AuthProvider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-  <BrowserRouter>
+  <BrowserRouter basename="/goit-react-hw-05-movies">
     <AuthProvider>
       <App />
     </AuthProvider>
